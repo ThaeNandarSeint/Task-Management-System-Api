f57d9fad328b9b6761829ea7a424432d60b101e8
ThaeNandarSeint <thaenandar14seint@gmail.com>
2024-09-14 14:43:20 +0630

    fix global error handler middleware

--- src/middlewares/error_handler.middleware.ts
+++ src/middlewares/error_handler.middleware.ts
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ApiError } from '../classes/error';
 import { sendFailedResponse } from '../utils';
 import { globalErrors } from '../errors';
 
-export const errorHandler = (error: Error, _: Request, res: Response) => {
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.log(error.stack);
   if (error instanceof ApiError) {
     sendFailedResponse({ res, error });
